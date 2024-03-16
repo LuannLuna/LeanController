cad46baded2836c4030e8b555882fd24164c003e
luannluna <luann.marques@gmail.com>
2024-03-16 11:33:06 -0300

    setting project

---
+++ LeanController/LeanController/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  LeanController
+//
+//  Created by Luann Luna on 15/03/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = TableViewViewController()
+
+        window.rootViewController = vc
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+
+}
---
+++ LeanController/LeanController/TableViewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeanController
+//
+//  Created by Luann Luna on 15/03/24.
+//
+
+import UIKit
+
+class TableViewViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
