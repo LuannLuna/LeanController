1060fdf51b81b5e9cb2e63144516fbc74bb1018f
luannluna <luann.marques@gmail.com>
2024-03-16 11:58:41 -0300

    integrating with CoreData

--- LeanController/LeanController/TableViewViewController.swift
+++ LeanController/LeanController/TableViewViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initializeCoreData()
     }
 
 
 }
 
+private
+extension TableViewViewController {
+    func initializeCoreData() {
+        guard let modelURL = Bundle.main.url(forResource: "MyGroceryDataModel", withExtension: "momd") else {
+            print("GrocryDataModel not found"); return
+        }
+
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            print("Unable to initialize ManagedObjectModel"); return
+        }
+
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+
+        let fileManager = FileManager()
+
+        guard let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Unable to get documents URL"); return
+        }
+
+        let storeURL = documentsURL.appendingPathComponent("MyGrocery.sqlite")
+
+        print(storeURL)
+
+//        try! persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+//
+//        let type = NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType
+//        self.managedObjectContext = NSManagedObjectContext(concurrencyType: type)
+//        self.managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+    }
+}
